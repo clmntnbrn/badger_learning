d3531a2f37bf073449bdbc8b85edcd953168ef0f
Joe Stanton <joe.stanton@red-badger.com>
2017-07-03 17:21:41 +0100

    Added some comments / arrow fns

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
-$(document).ready(function() {
+$(document).ready(() => {
   var inputs = document.querySelectorAll(".acc-group input");
+
+  // Keep track of the selected radio button, initially there isn't one
   var selected = null;
 
   inputs.forEach((i) => {
+    // When an input is clicked
     i.addEventListener("click", (e) => {
-      if (e.target === selected) {
-        e.target.checked = false;
-        selected = null;
-      } else {
+      // Is this a new selection
+      if (e.target !== selected) {
+        // yes, keep track of this selection for subsequent clicks
         selected = e.target;
+      } else {
+        // no, the user clicked the same thing twice, deselect instead
+        e.target.checked = false;
+        // track that we don't have anything selected anymore
+        selected = null;
       };
     });
   });
