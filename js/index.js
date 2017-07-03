9c72c0536eab5cb5a3f1292988885f8c62ff8241
sashaward <sasha.ward@red-badger.com>
2017-07-03 16:35:41 +0100

    wip

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 
-  $(".rotate").click(function(){
-      $(this).toggleClass("down");
+  var inputs = document.querySelectorAll(".acc-group input");
+  inputs[0].addEventListener("change", (e) => {
+    var clicked = e.target;
+
+    if (clicked.checked) {
+      clicked.checked = false;
+      console.log("Uncheck");
+    } else {
+      console.log("Check");
+    }
   });
 
 });
